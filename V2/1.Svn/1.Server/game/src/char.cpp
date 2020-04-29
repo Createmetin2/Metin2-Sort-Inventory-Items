7e3f82e4a4b08e20a2f48d2d6055e76a67576797
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2020-04-30 00:02:55 +0300

    Update char.cpp

--- V2/1.Svn/1.Server/game/src/char.cpp
+++ V2/1.Svn/1.Server/game/src/char.cpp
@@ -2,41 +2,46 @@
 #ifdef ENABLE_SORT_INVEN
 void CHARACTER::SortInven(BYTE option)
 {
-	if (IsDead()) return;
-	std::vector<LPITEM> all;
-    	LPITEM myitems;
-#ifdef ENABLE_EXTEND_INVEN_SYSTEM
-	auto size = Inventory_Size();
-#else
-	auto size = INVENTORY_MAX_NUM;
-#endif
+	if (IsDead()) 
+		return;
+	if (GetLastSortTime() > get_global_time()) {
+		ChatPacket(CHAT_TYPE_INFO, "You need to wait %d sec.", GetLastSortTime() - get_global_time());
+		return;
+	}
 
-	for (auto i = 0; i < size; ++i) {
+	std::vector<LPITEM> all;
+	LPITEM myitems;
+	const auto size = static_cast<WORD>(INVENTORY_MAX_NUM);
+
+	for (WORD i = 0; i < size; ++i) {
 		if (myitems = GetInventoryItem(i)) {
 			all.emplace_back(myitems);
 			myitems->RemoveFromCharacter();
-			SyncQuickslot(QUICKSLOT_TYPE_ITEM, i, 255);
+			SyncQuickslot(QUICKSLOT_TYPE_ITEM, static_cast<BYTE>(i), 255);
 		}
 	}
-	std::sort(all.begin(), all.end(), [option](const auto& i1, const auto& i2) {
+	if (all.empty())
+		return;
+	std::sort(all.begin(), all.end(), [option](const LPITEM i1, const LPITEM i2) {
 		switch (option) {
-			case 2:
-				return i1->CustomSort() == i2->CustomSort() ? i1->GetSubType() < i2->GetSubType() : i1->CustomSort() < i2->CustomSort();
-			break;
-			case 3:
-				return i1->GetLevelLimit() == i2->GetLevelLimit() ? i1->GetSubType() < i2->GetSubType() : i1->GetLevelLimit() > i2->GetLevelLimit();
-			break;
-			default:
-				return std::strcmp(i1->GetName(),i2->GetName()) < 0;
-			break;
+		case 2:
+			return i1->CustomSort() == i2->CustomSort() ? i1->GetSubType() < i2->GetSubType() : i1->CustomSort() < i2->CustomSort();
+		case 3:
+			return i1->GetLevelLimit() == i2->GetLevelLimit() ? i1->GetSubType() < i2->GetSubType() : i1->GetLevelLimit() > i2->GetLevelLimit();
+		default:
+			return std::strcmp(i1->GetName(), i2->GetName()) < 0;
 		}
 	});
 	for (const auto& getitem : all) {
-		const auto& table = ITEM_MANAGER::instance().GetTable(getitem->GetVnum());
-		if (table && table->dwFlags & ITEM_FLAG_STACKABLE && table->bType != ITEM_BLEND)
+		const auto table = ITEM_MANAGER::instance().GetTable(getitem->GetVnum());
+		if (!table)
+			continue;
+		static const std::initializer_list<BYTE> out = { ITEM_BLEND, ITEM_USE }; // edit
+		if (table->dwFlags & ITEM_FLAG_STACKABLE && std::find(out.begin(), out.end(), table->bType) == out.end())
 			AutoGiveItem(getitem->GetVnum(), getitem->GetCount(), -1, false);
 		else
 			AutoGiveItem(getitem);
-    	};
+	};
+	SetLastSortTime(get_global_time() + 15); // 15 sec
 }
 #endif
