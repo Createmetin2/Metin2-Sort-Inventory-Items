ec722d7139f72cd48bf9228585d0dc86daf6ac42
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2020-04-29 23:20:03 +0300

    Add files via upload

--- V2/2.Client/root/uiinventory.py
+++ V2/2.Client/root/uiinventory.py
@@ -1,3 +1,7 @@
+#Add
+if app.ENABLE_SORT_INVEN:
+	import uiToolTip
+	
 #Find
 			self.equipmentTab.append(self.GetChild("Equipment_Tab_02"))
 
@@ -5,7 +9,7 @@
 			if app.ENABLE_SORT_INVEN:
 				self.yenilebutton = self.GetChild2("YenileButton")
 				self.yenilebutton.SetEvent(ui.__mem_func__(self.ClickYenileButton))
-				self.tooltipI = uiToolTip.ItemToolTip()
+				self.tooltipI = uiToolTip.ToolTip()
 				self.tooltipI.Hide()
 				self.tooltipInfo = [self.tooltipI]*4
 				self.InformationText = [localeInfo.YENILE_BUTTON_TITLE,
@@ -13,7 +17,7 @@
 										localeInfo.YENILE_BUTTON2,
 										localeInfo.YENILE_BUTTON3
 				]
-				for i in xrange(4):
+				for i in xrange(len(self.tooltipInfo)):
 					self.tooltipInfo[i].SetFollow(True)
 					self.tooltipInfo[i].AlignHorizonalCenter()
 					if i == 0:
@@ -40,8 +44,8 @@
 	def OnUpdate(self):
 	
 #Add in
-		if app.ENABLE_SORT_INVEN:
-			for i in xrange(4):
+		if app.ENABLE_SORT_INVEN and self.tooltipInfo:
+			for i in xrange(len(self.tooltipInfo)):
 				if self.yenilebutton.IsIn():
 					self.tooltipInfo[i].Show()
 				else:
