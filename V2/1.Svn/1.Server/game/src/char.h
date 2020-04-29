4a4869a23401e8881effc0704a1a8a4537dc587b
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2020-04-30 00:02:31 +0300

    Update char.h

--- V2/1.Svn/1.Server/game/src/char.h
+++ V2/1.Svn/1.Server/game/src/char.h
@@ -4,4 +4,14 @@
 ///Add
 #ifdef ENABLE_SORT_INVEN
 		void			SortInven(BYTE option);
+		DWORD			GetLastSortTime() const { return m_dwLastSortTime; }
+		void			SetLastSortTime(DWORD time) { m_dwLastSortTime = time; }
+#endif
+
+//Find
+		bool				m_bDisableCooltime;
+
+///Add
+#ifdef ENABLE_SORT_INVEN
+		DWORD				m_dwLastSortTime;
 #endif
